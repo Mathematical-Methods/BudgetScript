77b0b9057d0efd362bf68e0f9513206485c56e8b
Unknown <unknown@GabesPC.attlocal.net>
2024-04-28 14:53:54 -0500

    Spelling corrections...

--- Budgetscript1.py
+++ Budgetscript1.py
@@ -89,68 +89,68 @@ Total = Bill_amount[0] + Debt_amount[0] + Housing_amount[0] + Insurance_amount[0
 with open("report.txt", "w") as f:
     if Income_amount[0] > 0:
         print("Income", file=f)
-        for reciept in Income:
-            print("     ", reciept, file=f);
+        for receipt in Income:
+            print("     ", receipt, file=f);
         print("    Income:",Income_amount[0], file=f);
     if Bill_amount[0] > 0:
         print("Bills", file=f)
-        for reciept in Bills:
-            print("     ", reciept, file=f);
+        for receipt in Bills:
+            print("     ", receipt, file=f);
         print("    Bills Cost:",Bill_amount[0], file=f)
     if Debt_amount[0] > 0:
         print("Debts", file=f)
-        for reciept in Debts:
-            print("     ", reciept, file=f);
+        for receipt in Debts:
+            print("     ", receipt, file=f);
         print("    Debts Cost:",Debt_amount[0], file=f);
     if Housing_amount[0] > 0:
         print("Housing", file=f)
-        for reciept in Housing:
-            print("     ", reciept, file=f);
+        for receipt in Housing:
+            print("     ", receipt, file=f);
         print("    Housing Cost:",Housing_amount[0], file=f);
     if Insurance_amount[0] > 0:
         print("Insurance", file=f)
-        for reciept in Insurance:
-            print("     ", reciept, file=f);
+        for receipt in Insurance:
+            print("     ", receipt, file=f);
         print("    Insurance Cost:",Insurance_amount[0], file=f);
     if Investment_amount[0] > 0:
         print("Investment", file=f)
-        for reciept in Investment:
-            print("     ", reciept, file=f);
+        for receipt in Investment:
+            print("     ", receipt, file=f);
         print("    Investment Cost:",Investment_amount[0], file=f);
     if Medical_amount[0] > 0:
         print("Medical", file=f)
-        for reciept in Medical:
-            print("     ", reciept, file=f);
+        for receipt in Medical:
+            print("     ", receipt, file=f);
         print("    Medical Cost:",Medical_amount[0], file=f);
     if MISC_amount[0] > 0:
         print("MISC", file=f)
-        for reciept in MISC:
-            print("     ", reciept, file=f);
+        for receipt in MISC:
+            print("     ", receipt, file=f);
         print("    MISC Cost:",MISC_amount[0], file=f);
     if Personal_amount[0] > 0:
         print("Personal", file=f)
-        for reciept in Personal:
-            print("     ", reciept, file=f);
+        for receipt in Personal:
+            print("     ", receipt, file=f);
         print("    Personal Cost:",Personal_amount[0], file=f);
     if Saving_amount[0] > 0:
         print("Saving", file=f)
-        for reciept in Saving:
-            print("     ", reciept, file=f);
+        for receipt in Saving:
+            print("     ", receipt, file=f);
         print("    Saving Cost:",Saving_amount[0], file=f);
     if Transportation_amount[0] > 0:
         print("Transportation", file=f)
-        for reciept in Transportation:
-            print("     ", reciept, file=f);
+        for receipt in Transportation:
+            print("     ", receipt, file=f);
         print("    Transportation Cost:",Transportation_amount[0], file=f);
     if Utilities_amount[0] > 0:
         print("Utilities", file=f)
-        for reciept in Utilities:
-            print("     ", reciept, file=f);
+        for receipt in Utilities:
+            print("     ", receipt, file=f);
         print("    Utilities Cost:",Utilities_amount[0], file=f);
     if Food_amount[0] > 0:
         print("Food", file=f)
-        for reciept in Food:
-            print("     ", reciept, file=f);
+        for receipt in Food:
+            print("     ", receipt, file=f);
         print("    Food Cost:",Food_amount[0], file=f);
 
     print('You earned $',Income_amount[0],'this month.', file=f);
