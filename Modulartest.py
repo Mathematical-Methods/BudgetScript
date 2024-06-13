7ddba30949e4d043240960f53f609be6be5bd3ce
unknown <mathematicalmethods@protonmail.com>
2024-06-12 21:32:40 -0500

    New file, experimenting with lists in lists, and for loops for variable replacement.

---
+++ Modulartest.py
@@ -0,0 +1,26 @@
+import os
+
+# get the working directory
+workingdir = os.getcwd();
+# file name retrieval: os.listdir returns a list of files within the spec'd directory
+receipts = os.listdir(workingdir);
+
+# Create a list of all variable names
+Income = [[""],[0]]; 
+# "" is a placeholder for the names of the receipts. It is (believed) to be equiv. to Income = []. 
+Bills = [[""],[0]];
+# 0 is a placeholder for the initial amount of the receipts. It is to be equiv. to Income = [].
+Debts = [[""],[0]];
+Housing = [[""],[0]];
+
+variable_list = [Income,Bills,Debts,Housing];
+
+for receipt in receipts:
+    receipt = receipt.replace('.pdf','')
+    receipt = receipt.replace('.PDF','')
+    fields = receipt.split(" ",3)
+    if len(fields) == 4:
+        for variable in variable_list:
+            if fields[3].split('-',1)[0] == "Income" or fields[3].split('-',1)[0] == "Return" or fields[3].split('-',1)[0] == "Payroll":
+                variable[0][0].append(receipt);
+                variable[1][0] = variable[1][0] + float(fields[1].strip("+$"));
