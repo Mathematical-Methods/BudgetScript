e1ab46d5ee61a0391480c415fdec6f42ed547347
unknown <mathematicalmethods@protonmail.com>
2024-06-14 16:36:07 -0500

    Updated Modulartest.py

--- Modulartest.py
+++ Modulartest.py
@@ -3,23 +3,30 @@ import os
 # get the working directory
 workingdir = os.getcwd();
 # file name retrieval: os.listdir returns a list of files within the spec'd directory
-receipts = os.listdir(workingdir);
+receipts = os.listdir('/home/unknown/Downloads/05. May/');
+#receipts = os.listdir(workingdir);
 
-# Create a list of all variable names
-Income = [[""],[0]]; 
+# Create a dictionary of all variables names
 # "" is a placeholder for the names of the receipts. It is (believed) to be equiv. to Income = []. 
-Bills = [[""],[0]];
 # 0 is a placeholder for the initial amount of the receipts. It is to be equiv. to Income = [].
-Debts = [[""],[0]];
-Housing = [[""],[0]];
+config = {"Income"  : [["Income"],[0]],
+          "Return"  : [["Income"],[0]],
+          "Payroll" : [["Income"],[0]],
+          "Bill"    : [["Bill"],[0]]};
 
-variable_list = [Income,Bills,Debts,Housing];
+#totalSpent = 0;
+#totalEarned = 0;
 
 for receipt in receipts:
-    receipt = receipt.replace('.pdf','')
-    receipt = receipt.replace('.PDF','')
-    fields = receipt.split(" ",3)
+    receipt = receipt.lower().replace('.pdf','');
+    fields = receipt.split(" ",3);
     if len(fields) == 4:
-            if fields[3].split('-',1)[0] == "Income" or fields[3].split('-',1)[0] == "Return" or fields[3].split('-',1)[0] == "Payroll":
-                variable[0][0].append(receipt);
-                variable[1][0] = variable[1][0] + float(fields[1].strip("+$"));
+        Category = config.get(fields[3].split('-',1)[0])
+        if type(Category).__name__ != 'NoneType':
+            Category[0].append(receipt);
+            Category[1][0] = Category[1][0] + float(fields[1].strip("+$"));
+## Total spent.
+            #if Category[0][0] != 'Income':
+            #    totalSpent = totalSpent + Category[1][0];
+            #else 
+            #    totalEarned = totalEarned + Category[1][0];
