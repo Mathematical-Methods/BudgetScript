09f1b596db7d43dd4e39b4e1b1666fde7c0e3f73
unknown <mathematicalmethods@protonmail.com>
2024-07-13 14:58:39 -0500

    Added Data proprocessing script

---
+++ datamaker.py
@@ -0,0 +1,25 @@
+import os # os is a library for interacting with the file system.
+
+
+# define category function-> input: individual file name; output: category of file or "Unlisted category" and file name.
+def category_function(file):
+
+
+
+    # define unlisted category function-> input: "Unlisted category" and file name.; output: file moved to an unlisted category folder.
+
+
+# define OCR function-> input: individual file name; output: text in the file.
+
+    # define text processor function-> input: text in read file; output: text with spaces between words, punctuation, and no carriage return "\n" characters. 
+
+
+
+
+
+
+# define main function-> input: category and text; output: added row to csv file with category and text in respective columns.
+
+# get a list of files in specified folder.
+
+# for each file in list of files.
