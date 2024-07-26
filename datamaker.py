25ddc3c38e929a6f4b3357c1f6d98aea5a69bd96
Mathematical-Methods <138537308+Mathematical-Methods@users.noreply.github.com>
2024-07-25 21:15:35 -0500

    Adding preprocessing calls to preprocess()

--- datamaker.py
+++ datamaker.py
@@ -6,7 +6,7 @@ pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 import cv2
 import numpy as np
 
-
+tesseract_config = "--oem 3 --dpi 300 --psm 3 -l eng+osd"
 receipt_database = "/home/unknown/Documents/00.Repositories/BudgetScript/receipt_database/"
 unlisted_database = "/home/unknown/Documents/00.Repositories/BudgetScript/unlisted_database/"
 receipts = os.listdir(receipt_database)
@@ -30,26 +30,43 @@ config = {"income"              : [["Income"],[0]],
           "donation"            : [["Donation"],[0]],
           };
 
-def erosion_dilation(image):
+def preprocess(image):
+# GreyScale Method
+    image = image.convert('L')
+# Turn image into array
     image = np.asarray(image)
-    # Creating kernel
-    kernelhoriz = np.array([[0,0,0,0,0],
-                            [0,0,0,0,0],
-                            [1,1,1,1,1],
-                            [0,0,0,0,0],
-                            [0,0,0,0,0]], np.uint8) 
-    kernelvert = np.array([[0,0,1,0,0],
-                           [0,0,1,0,0],
-                           [0,0,1,0,0],
-                           [0,0,1,0,0],
-                           [0,0,1,0,0]], np.uint8) 
+    print("This is the array shape of the receipt image before preprocessing:",image.shape)
+# Resizing the image method
+    up_size=(2*image.shape[1],2*image.shape[0])
+    image = cv2.resize(image,up_size,interpolation= cv2.INTER_LINEAR)
+    print("This is the array shape of the receipt image after:",image.shape)
+# Using cv2.erode() & dilate method to remove noise
+### Creating Kernel
     kernele = np.ones((3, 3), np.uint8)
-    # Using cv2.erode() method
-    image = cv2.erode(image, kernelhoriz)
-    image = cv2.erode(image,kernelvert)
+    image = cv2.erode(image, kernele)
     image = cv2.dilate(image, kernele)
-    image = Image.fromarray(image, 'RGB')
+
+# Turn array back into PIL Image    
+    image = Image.fromarray(image, 'L')
     return image
+
+# Deskewing
+    ## https://github.com/kakul/Alyn
+# Scanning border removal
+    ## https://stackoverflow.com/questions/57858944/opencv-python-border-removal-preprocessing-for-ocr
+# see the below libraries:
+    ## https://tesseract-ocr.github.io/tessdoc/ImproveQuality.html#noise-removal
+    ## Leptonica
+    ## OpenCV
+    ## ScanTailor Advanced
+    ## ImageMagick
+    ## unpaper
+    ## ImageJ
+    ## Gimp
+    ## PRLib - Pre-Recognize Library with algorithms for improving OCR quality
+# Dictionaries, word lists, patterns.
+    ## Disabling the dictionaries Tesseract uses should increase recognition if most of your text isn’t dictionary words. They can be disabled by setting both of the configuration variables load_system_dawg and load_freq_dawg to false.
+    
 
 def category_function(file):
     receipt_name = file.lower().replace('.pdf','').replace('.jpg','').replace('.jpeg','').split(" ",3)
@@ -65,37 +82,40 @@ def unlisted_filter(file,receipt_category,dict):
         #os.rename(receipt_database+'/'+file, unlisted_database+'/'+file)
 
 # define OCR function-> input: individual file name; output: text in the file.
-def OCR(file):
+def OCR(file,inc):
     ind = ""
     if ".pdf" in file:
         receipt_pages = convert_from_path(receipt_database+file);
         ind = ".pdf"
         receipt_text_pages = ""
         for page in receipt_pages:
-            page = erosion_dilation(page)
-            receipt_text_page=pytesseract.image_to_string(page)
+
+            page = preprocess(page)
+            receipt_text_page=pytesseract.image_to_string(page, config=tesseract_config)
             receipt_text_pages+=receipt_text_page
-        page.save('output_image.jpg')
+        page.save('./tesseractoutput/output_image'+str(inc)+'.jpg')
         print(receipt_text_pages)
         return receipt_text_pages
     else:
         image = receipt_database+'/'+file
-        image = erosion_dilation(image)
-        receipt_text=pytesseract.image_to_string(image)
+        image = preprocess(image)
+        receipt_text=pytesseract.image_to_string(image, config=tesseract_config)
         print(receipt_text)
-        image.save('output_image.jpg')
+        image.save('./tesseractoutput/output_image'+str(inc)+'.jpg')
         return receipt_text 
+
+
 
 i=0
 for receipt in receipts:
     receipt_category = category_function(receipt)
-    print(receipt_category)
+    print("The category is: ", receipt_category)
     receipt_category = unlisted_filter(receipt,receipt_category,config)
     if receipt_category != None:
-        print(receipt_category)
-        OCR(receipt)
+        print("For redundancy, the category is: ", receipt_category)
+        OCR(receipt,i)
     i+=1
-    if i == 4: 
+    if i == 7: 
         break
 
 
